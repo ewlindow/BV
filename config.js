b92134c57044d1d05da26864f7cb018ff2934f99
ewlindow <95576916+ewlindow@users.noreply.github.com>
2026-07-25 20:39:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ window.BOURBON_VAULT_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbySmMxtPh1LAtzvxhN9lHluemuBSq_C6Jr55OP_9g6j8xXUWmMn3V1-H6rD5OeMMmNSWg/exec",
   useFallbackData: true
 };
+
+console.log("Bourbon Vault config loaded:", window.BOURBON_VAULT_CONFIG);
